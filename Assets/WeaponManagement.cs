a18dcb7bc1b93aea3563ebbec2842e68ddc1fded
kennhyfeng <129352181+kennhyfeng@users.noreply.github.com>
2023-05-25 11:58:07 +0800

    5.25

--- Assets/WeaponManagement.cs
+++ Assets/WeaponManagement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;  // 要有這個才能控制文字框
 
-public class Shooting: MonoBehaviour
+public class WeaponManagement : MonoBehaviour
 {
     [Header("參考物件")]
     public Camera PlayerCamera;
@@ -18,13 +18,17 @@ public class Shooting: MonoBehaviour
     public float reloadTime;        // 設定換彈夾所需要的時間
     public float recoilForce;       // 反作用力
 
-    bool reloading;             // 布林變數：儲存是不是正在換彈夾的狀態？True：正在換彈夾、False：換彈夾的動作已結束
+    bool reloading;                 // 布林變數：儲存是不是正在換彈夾的狀態？True：正在換彈夾、False：換彈夾的動作已結束
 
     [Header("UI物件")]
     public TextMeshProUGUI ammunitionDisplay; // 彈量顯示
     public TextMeshProUGUI reloadingDisplay;  // 顯示是不是正在換彈夾？
 
-    public Animator animatorObject;  // 動畫播放器組件
+    [Header("武器")]
+    public GameObject[] weaponObjects;        // 武器清單
+
+    int weaponNumber = 0;                     // 目前選擇武器的順序編號
+    GameObject weaponInUse;                   // 目前選擇武器
 
     private void Start()
     {
@@ -32,6 +36,7 @@ public class Shooting: MonoBehaviour
         reloadingDisplay.enabled = false;  // 將顯示正在換彈夾的字幕隱藏起來
 
         ShowAmmoDisplay();                 // 更新彈量顯示
+        weaponInUse = weaponObjects[0];    // 遊戲一開始設定武器為第0個武器
     }
 
     private void Update()
@@ -55,6 +60,20 @@ public class Shooting: MonoBehaviour
         // 判斷：1.有按下R鍵、2.子彈數量低於彈夾內的彈量、3.不是換彈夾的狀態，三個條件都滿足，就可以換彈夾
         if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading)
             Reload();
+
+        // 判斷：按下數字鍵1，切換為武器0
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            SwitchWeapon(0, 0);
+
+        // 判斷：按下數字鍵2，切換為武器1
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            SwitchWeapon(0, 1);
+
+        // 判斷：滾動滑鼠滾輪
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)      // 往前滾動
+            SwitchWeapon(1);
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // 往後滾動
+            SwitchWeapon(-1);
     }
 
     // 方法：射擊武器
@@ -88,7 +107,7 @@ public class Shooting: MonoBehaviour
 
         ShowAmmoDisplay();                 // 更新彈量顯示
 
-        animatorObject.SetTrigger("Fire");  // 觸發「Fire」的觸發變數
+        weaponInUse.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Fire");  // 觸發「Fire」的觸發變數
     }
 
     // 方法：換彈夾的延遲時間設定
@@ -113,5 +132,39 @@ public class Shooting: MonoBehaviour
     {
         if (ammunitionDisplay != null)
             ammunitionDisplay.SetText($"Ammo {bulletsLeft} / {magazineSize}");
+    }
+
+    // 方法：武器切換，參數_addNumber、_weaponNumber
+    private void SwitchWeapon(int _addNumber, int _weaponNumber = 0)
+    {
+        // 將武器清單全部隱藏，先一次全部隱藏，再顯示需要的武器
+        foreach (GameObject item in weaponObjects)
+        {
+            item.SetActive(false);
+        }
+
+        // switch 判斷式：以參數_addNumber判斷要怎麼切換武器
+        switch (_addNumber)
+        {
+            case 0:                                                   // _addNumber == 0，代表用按鍵直接指定武器陣列位址
+                weaponNumber = _weaponNumber;
+                break;
+            case 1:                                                   // _addNumber == 1，代表往上滾滑鼠滾輪
+                if (weaponNumber == weaponObjects.Length - 1)         // 實現循環數字，假定原本的武器陣列位址已經是最後一個武器，則將武器陣列位址設定為0
+                    weaponNumber = 0;
+                else
+                    weaponNumber += 1;
+                //weaponNumber = (weaponNumber == weaponObjects.Length - 1) ? 0 : weaponNumber += 1; // 也可以把以上的判斷式寫成這樣
+                break;
+            case -1:                                                   // _addNumber == -1，代表往下滾滑鼠滾輪
+                if (weaponNumber == 0)                                 // 實現循環數字，假定原本的武器陣列位址是第一個武器，則將武器陣列位址為清單的最後一個位址
+                    weaponNumber = weaponObjects.Length - 1;
+                else
+                    weaponNumber -= 1;
+                //weaponNumber = (weaponNumber == 0) ? weaponObjects.Length - 1 : weaponNumber -= 1; // 也可以把以上的判斷式寫成這樣
+                break;
+        }
+        weaponObjects[weaponNumber].SetActive(true);    // 顯示所指定的武器
+        weaponInUse = weaponObjects[weaponNumber];      // 設定目前所選擇的武器物件(屆時可以用來執行武器所特定的方法，下一章節會介紹)
     }
 }
